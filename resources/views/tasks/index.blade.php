2f77ceae1c3c3f6b83284cb86c6de4e1c08cd352
kb995 <t.kuboshima995@gmail.com>
2020-08-16 06:49:26 +0900

    Folder 追加

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -20,8 +20,8 @@
         <nav class="panel panel-default">
           <div class="panel-heading">フォルダ</div>
           <div class="panel-body">
-            <a href="#" class="btn btn-default btn-block">
-              フォルダを追加する
+            <a href="{{ route('folders.create') }}" class="btn btn-default btn-block">
+                フォルダを追加する
             </a>
           </div>
           <div class="list-group">
