8ec11da3ac3ca2bb68f04488fb5d840c2ce42f15
kb995 <t.kuboshima995@gmail.com>
2020-08-16 09:38:36 +0900

    chapter06

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// 一覧表示
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
-
+// フォルダ作成
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'FolderController@create');
+// タスク作成
+Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', 'TaskController@create');
